b91853543383e045d19d597bde4dcf68b2920f1e
namuan <575441+namuan@users.noreply.github.com>
2023-01-02 13:16:02 +0000

    refactor: Improve as suggested

--- multi-repo/pending-commit.sh
+++ multi-repo/pending-commit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-cd $HOME/workspace
+cd "$HOME"/workspace || exit
 ./namuan/multi-repo/batch-run.py "git status | grep modified && idea . && echo 'Press ENTER to continue' && read"
--- multi-repo/pending-outgoing.sh
+++ multi-repo/pending-outgoing.sh
@@ -2,5 +2,5 @@
 
 # git alias outgoing "log --abbrev-commit --graph @{u}.."
 
-cd $HOME/workspace
+cd "$HOME"/workspace || exit
 ./namuan/multi-repo/batch-run.py "git --no-pager outgoing | grep commit && idea . && echo 'Press ENTER to continue' && read"
