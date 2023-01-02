8a79ff17768982f8e7a436c4625cdc52427c3818
namuan <575441+namuan@users.noreply.github.com>
2023-01-02 14:56:11 +0000

    refactor: shellcheck

--- multi-repo/clone-all.sh
+++ multi-repo/clone-all.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
-cd $HOME/workspace
+cd "$HOME"/workspace || exit
 
-curl -s https://api.github.com/users/namuan/repos\?per_page\=100 | jq -r ".[].ssh_url" | while read line; do
-    repo_name=$(basename $line ".git")
-    if test ! -d $repo_name; then
-        git clone $line
+curl -s https://api.github.com/users/namuan/repos\?per_page\=100 | jq -r ".[].ssh_url" | while read -r line; do
+    repo_name=$(basename "$line" ".git")
+    if test ! -d "$repo_name"; then
+        git clone "$line"
     fi
 done
