92c3254fe208898e4e079588b55d47ae42236520
namuan <575441+namuan@users.noreply.github.com>
2023-01-02 12:49:42 +0000

    feat: Added section to manage multiple git repos

---
+++ multi-repo/README.md
@@ -0,0 +1,4 @@
+# Multi Repo Scripts
+
+This is a collection of scripts to help with managing multiple git repositories.
+
---
+++ multi-repo/clone-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+cd $HOME/workspace
+
+curl -s https://api.github.com/users/namuan/repos\?per_page\=100 | jq -r ".[].ssh_url" | while read line; do
+    repo_name=$(basename $line ".git")
+    if test ! -d $repo_name; then
+        git clone $line
+    fi
+done
